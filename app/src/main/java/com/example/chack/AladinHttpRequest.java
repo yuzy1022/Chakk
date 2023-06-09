904ea4cb52034f93d8313edaf76f6d5b42053f1f
psw <nnayu526@gmail.com>
2023-06-09 11:49:00 +0900

    api로 책검색과 책조회하는거 약간 수정

--- app/src/main/java/com/example/chack/AladinHttpRequest.java
+++ app/src/main/java/com/example/chack/AladinHttpRequest.java
@@ -12,19 +12,21 @@ public interface AladinHttpRequest {
     // getName > 메소드 명. 자유롭게 설정 가능, 통신에 영향x
     // @Path("post") String post > 매개변수. 매개변수 post가 @Path("post")를 보고 @GET 내부 {post}에 대입
     @GET("ItemSearch.aspx")
-    Call<DataClass> getSearchBook(
-            @Query("ttbkey") String ttbkey,
-            @Query("Query") String Query,
-            @Query("SearchTarget") String searchTarget,
-            @Query("output") String output,
-            @Query("Version") String version
+    Call<DataClass> getSearchBook(  //상품검색
+            @Query("ttbkey") String ttbkey,  //ttb키 입력
+            @Query("Query") String Query,  //검색할 검색어
+            @Query("SearchTarget") String searchTarget, //검색대상 (Book으로 입력)
+            @Query("MaxResults") String maxResult,  //검색 결과 최대 출력 개수 (최대 100)
+            @Query("output") String output,  //출력 방법 (js로 입력)
+            @Query("Version") String version  //버전 (20131101 입력)
     );
 
-    Call<DataClass> getISBNSearchBook(
-            @Query("ttbkey") String ttbkey,
-            @Query("Query") String Query,
-            @Query("output") String output,
-            @Query("Version") String version,
-            @Query("ItemIdType") String itemIdType
+    @GET("ItemLookUp")
+    Call<DataClass> getISBNSearchBook( //상품 조회
+            @Query("ttbkey") String ttbkey, //ttb키 입력
+            @Query("ItemId") String itemId, // ItemIdType에서 정한 타입으로 검색할 값 입력
+            @Query("ItemIdType") String itemIdType, //isbn, isbn13 중 택
+            @Query("output") String output,  //출력 방법 (js로 입력)
+            @Query("Version") String version  //버전 (20131101 입력)
     );
 }

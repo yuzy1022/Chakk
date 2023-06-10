a58dd2409d50249615e59932f0d657d95120061d
psw <nnayu526@gmail.com>
2023-06-11 07:17:58 +0900

    필요없는 코드 삭제

--- app/src/main/java/com/example/chack/recordFragment.java
+++ app/src/main/java/com/example/chack/recordFragment.java
@@ -77,16 +77,6 @@ public class recordFragment extends Fragment {
         //내서재 xml로 뷰객체 생성
         View v = inflater.inflate(R.layout.fragment_record, container, false);
 
-        /*//fragment_record  xml안에 viewpager가져와 뷰페이저에 넣음
-        viewPager = v.findViewById(R.id.viewpager);
-        //뷰페이저에 어댑터 설정
-        recordViewPagerAdapter adapter = new recordViewPagerAdapter(getActivity().getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-
-        //탭 레이아웃 가져와 뷰페이저 설정해줌
-        TabLayout tabLayout = v.findViewById(R.id.tab_layout);
-        tabLayout.setupWithViewPager(viewPager);*/
-
         viewPager2 = v.findViewById(R.id.viewpager);
         tabLayout = v.findViewById(R.id.tab_layout);
 
@@ -117,55 +107,6 @@ public class recordFragment extends Fragment {
         return v;
     }
 }
-
-
-/*//내서재 탭바 프래그먼트에 쓸 어댑터 클래스
-class recordViewPagerAdapter extends FragmentPagerAdapter
-{
-    private ArrayList<Fragment> arrayList = new ArrayList<>();  //프래그먼트를 담을 리스트
-    private ArrayList<String> name = new ArrayList<>();  //탭 이름을 넣을 리스트
-
-    //생성자
-    public recordViewPagerAdapter(@NonNull FragmentManager fm) {
-        super(fm);
-
-        //arrayList에 각 프래그먼트 클래스 추가
-        arrayList.add(new pastRecordFragment());
-        arrayList.add(new currentRecordFragment());
-        arrayList.add(new futureRecordFragment());
-        arrayList.add(new memoRecordFragment());
-
-        //name 리스트에 탭바 이름으로 쓸 문자열 추가
-        name.add("읽은 책");
-        name.add("읽고 있는 책");
-        name.add("읽고 싶은 책");
-        name.add("메모");
-    }
-
-
-    //position을 입력받아 name에서 position번쨰의 탭이름을 리턴해주는 메소드
-    @Override
-    public CharSequence getPageTitle(int position)
-    {
-        return name.get(position);
-    }
-
-    //position을 입력받아 arrayList에서 position번째의 프래그먼트를 리턴해주는 메소드
-    @NonNull
-    @Override
-    public Fragment getItem(int position)
-    {
-        return arrayList.get(position);
-    }
-
-    //arrayList에 있는 프래그먼트 개수를 리턴해주는 메소드
-    @Override
-    public int getCount()
-    {
-        return arrayList.size();
-    }
-}*/
-
 
 
 //뷰페이저2 어댑터 클래스

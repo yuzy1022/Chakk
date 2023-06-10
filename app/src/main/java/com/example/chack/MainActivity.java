749b86f78f00f0c2b3f48aef6946f353fedf4f6d
SongHyeongJin <hjsong1102@gmail.com>
2023-06-10 20:11:09 +0900

    로그인 후 화면이 설정으로 나오는 것을 수정

--- app/src/main/java/com/example/chack/MainActivity.java
+++ app/src/main/java/com/example/chack/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         nickname = intent.getStringExtra("nickname");
 
+
         init(); //네비게이션 버튼, 객체 생성
         SettingListener(); //네비게이션 버튼 설정
 
@@ -33,10 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         profileFragment fragment = profileFragment.newInstance(nickname);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame, fragment);
         transaction.commit();
         //페이지마다 액션바 이름 바꾸기
     }
+
 
     private void init() {
         frame = findViewById(R.id.frame);
@@ -71,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                     getSupportActionBar().setTitle(R.string.actionProfileName);
                     return true;
                 }
+
             }
             return false;
         }
